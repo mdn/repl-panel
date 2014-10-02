f89aa27eb706352bf89e37598d71dace7eb36b05
Will Bamberg <wbamberg@mozilla.com>
2014-10-02 10:20:46 +0100

    very simplified version

--- data/repl-panel.js
+++ data/repl-panel.js
@@ -4,27 +4,13 @@ window.addEventListener("message", function(event) {
 });
 
 var request = document.getElementById("request");
-var lastRequest = document.getElementById("last-request");
-var JSONError = document.getElementById("json-error");
 var response = document.getElementById("response");
 
 request.addEventListener("keyup", send, false);
 
 function send(event) {
   if ((event.keyCode == 13) && (event.getModifierState("Control") == true)) {
-    JSONError.textContent = "";
-    lastRequest.textContent = "";
-    response.textContent = "";
-
-    try {
-        var requestContent = JSON.parse(request.value);
-        window.port.postMessage(requestContent);
-        lastRequest.textContent = JSON.stringify(requestContent, undefined, 2);
-        request.value = "";
-    }
-    catch (exception) {
-        JSONError.textContent = exception.message;
-    }
+    window.port.postMessage(JSON.parse(request.value));
   }
 }
 
